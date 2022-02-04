627c0ff845357c9d8847bae330e458838667685a
The-Gentleman <zraja4402@gmail.com>
2022-02-03 19:38:43 -0500

    what I have isnt working. Going to try to change the default

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,9 +3,10 @@
 class Task < ApplicationRecord
   include AutoCompletable
   belongs_to :task_list
-  validates :description, :complete inclusion: { in: [true, false] }, presence: true
+  validates :description, :complete, inclusion: { in: [true, false] }, presence: true
   #I found another bug here. I'm wondering if I should figure out how
   #to adjust this validation or just run a migration to change 'complete's
   #default to 'true'.
+  # inclusion: { in: [true, false] } does not seem to be working
 
 end
